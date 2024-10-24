d02f22ee0d318941dca69635a2cdfdf0439bb65d
juha <sjh990715@gmail.com>
2024-10-24 22:06:40 +0900

    week6/과제

---
+++ week6/draganddrop.js
@@ -0,0 +1,47 @@
+let highestZIndex = 10;
+let topPlant = null; // 최상단 요소를 추적
+
+let offsetX = 0;
+let offsetY = 0;
+
+function dragStart(e) {
+  const plant = e.target;
+
+  offsetX = e.clientX - plant.offsetLeft;
+  offsetY = e.clientY - plant.offsetTop;
+
+  e.dataTransfer.setData("text/plain", e.target.id);
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function drop(e) {
+  e.preventDefault();
+
+  const id = e.dataTransfer.getData("text");
+  const plant = document.getElementById(id);
+  const newLeft = e.clientX - offsetX;
+  const newTop = e.clientY - offsetY;
+
+  console.log(newLeft);
+  console.log(newTop);
+
+  plant.style.left = `${newLeft}px`;
+  plant.style.top = `${newTop}px`;
+}
+
+function bringToFront(e) {
+  highestZIndex++;
+  e.target.style.zIndex = highestZIndex;
+  topPlant = e.target.id;
+}
+
+document.querySelectorAll(".plant").forEach((plant) => {
+  plant.addEventListener("dragstart", dragStart);
+  plant.addEventListener("dblclick", bringToFront);
+});
+
+document.body.addEventListener("dragover", dragOver);
+document.body.addEventListener("drop", drop);
